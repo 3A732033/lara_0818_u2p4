870da6b32fc5d5cea7ac26b4d526d71e1f96054e
李譯騏 <patrick14754@gmail.com>
2020-12-04 00:55:18 +0800

    更改主樣板，子樣板

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -1,24 +1,24 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+        <a class="navbar-brand" href="index.blade.php">Start Bootstrap</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-                <li>
-                    <a href="{{route('posts.index')}}">Home</a>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.blade.php">Home</a>
                 </li>
-                <li>
-                    <a href="{{route('posts.about')}}">About</a>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.blade.php">About</a>
                 </li>
-                <li>
-                    <a href="{{route('posts.show')}}">Sample Post</a>
+                <li class="nav-item">
+                    <a class="nav-link" href="post.blade.php">Sample Post</a>
                 </li>
-                <li>
-                    <a href="{{route('posts.contact')}}">Contact</a>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.blade.php">Contact</a>
                 </li>
             </ul>
         </div>
